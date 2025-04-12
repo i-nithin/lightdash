47f92e4aaea5c4e0871f7bec3a72e21abfb212b3
i-nithin <nithinkmr2001@gmail.com>
2025-04-12 13:40:33 +0530

    init commit

--- saas_analytics/models/core/monthly_churn.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='view') }}
-
-select
-    date_trunc('month', end_date) as month,
-    count(*) as churned_subscriptions
-from {{ ref('stg_subscriptions') }}
-where end_date is not null
-group by 1
-order by month
--- saas_analytics/models/core/monthly_signups.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='view') }}
-
-select
-    date_trunc('month', signup_date) as month,
-    source,
-    count(*) as signups
-from {{ ref('stg_users') }}
-group by 1, 2
-order by month
